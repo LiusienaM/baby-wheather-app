0999b6368a08d46538623eb5257c91f31de0b471
Liusiena Martyniuk <lusenamartynuk@gmail.com>
2022-10-20 14:36:33 +0300

    Add Js properties for London

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#tempNow");
   let cityElement = document.querySelector("#cityName");
+  let descriptionElement = document.querySelector("#weatherNow");
+  let humidityElement = document.querySelector("#humidityPercent");
+  let windElement = document.querySelector("#windSpeed");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "499ae2429cb698163d7523b3dadcc6ef";
